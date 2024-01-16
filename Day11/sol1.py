7d5aebb813226f971ae4969970894c25f01a3022
akash <akash.verma2580@gmail.com>
2024-01-16 23:57:42 +0530

    [day 11] solution

--- Day11/sol1.py
+++ Day11/sol1.py
@@ -1,29 +1,28 @@
 def findLargestMinDistance(board:list, k:int):
 
-    def total(arr, frm, to):
+    def sum(board, frm, to):
         total = 0
         for i in range(frm, to + 1):
-            total += arr[i]
+            total += board[i]
         return total
 
-    def helper(arr, n, k, memo):
-        if k == 1:
-            return total(arr, 0, n - 1)
-        if n == 1:
-            return arr[0]
+    n=len(board)
+    prefix_sum = [0] * (n + 1)
 
-        if (n, k) in memo:
-            return memo[(n, k)]
+    for i in range(1, n + 1):
+        prefix_sum[i] = prefix_sum[i - 1] + board[i - 1]
 
-        best = float('inf')
+    dp = [[float('inf')] * (k + 1) for _ in range(n + 1)]
 
-        for i in range(1, n + 1):
-            best = min(best, max(helper(arr, i, k - 1, memo), total(arr, i, n - 1)))
+    for i in range(1, n + 1):
+        dp[i][1] = prefix_sum[i]
 
-        memo[(n, k)] = best
-        return best
+    for i in range(1, k + 1):
+        dp[1][i] = board[0]
 
+    for i in range(2, n + 1):
+        for j in range(2, k + 1):
+            for p in range(1, i):
+                dp[i][j] = min(dp[i][j], max(dp[p][j - 1], prefix_sum[i] - prefix_sum[p]))
 
-    memo = {}
-    return helper(board, len(board), k, memo)
-
+    return dp[n][k]
